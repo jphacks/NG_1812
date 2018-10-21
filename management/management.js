dbebdbc5e8bf5c2758def98eaf1046f34746daad
Fumiya Kume <fumiya.kume@hotmail.com>
2018-10-21 12:06:06 +0900

    ログイン処理後に飛んできたCode をCookie に保存するようにした

--- management/management.js
+++ management/management.js
@@ -11,6 +11,21 @@ class Management extends React.Component {
   }
 
   componentDidMount = () => {
+    const getQuery = () => {
+      if(window.location.search === "") return;
+      const variables = window.location.search.split("?")[1].split("&");
+      const obj = {};
+      variables.forEach(function(v, i) {
+          const variable = v.split("=");
+          obj[variable[0]] = Number(variable[1]);
+      });
+      return obj;
+
+      const code = getQuery().code;
+      document.cookie = `accessToken=${code}`
+  }
+  
+
     //this.getUserIcon(this.state.git_user_url);
   };
 
